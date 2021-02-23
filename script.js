5c6f3767e636c095fbe277f322b9e04722aa5e7c
thiyagesh kanna <thiyagesh14092000@gmail.com>
2021-02-24 03:34:40 +0530

    add

--- script.js
+++ script.js
@@ -79,6 +79,5 @@ document.querySelector(".nav-links").addEventListener("click", (e) => {
   if (e.target.classList.contains("nav-link")) {
     const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-    console.log(id);
   }
 });
